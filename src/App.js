26ab4a7e589e1c88ee43467107a690021f1b8e37
Sina Kiamehr Work <sinakiamehr7@email.com>
2025-03-31 16:00:15 -0700

    fixed all styling issues, ready for vercel deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { marked } from "marked";
+import "./App.css";
+import React from "react";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+  const initialText = `
+# Heading 1
+
+## Subheading (H2)
+
+[This is a link](https://www.example.com)
+
+Here is some \`inline code\`.
+
+\`\`\`
+This is a code block
+with multiple lines.
+\`\`\`
+
+- This is a list item.
+
+> This is a blockquote.
+
+![Alt text for image](https://via.placeholder.com/150)
+
+**This is bolded text.**
+`;
+  const handleChange = (e) => {
+    const output = e.target.value;
+    setText(output);
+  };
+
+  useEffect(() => {
+    setText(initialText);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <label for="editor">Editor</label>
+        <textarea
+          id="editor"
+          type="text"
+          onChange={handleChange}
+          value={text}
+        ></textarea>
+        <label for="preview">Previewer</label>
+        <textarea id="preview" value={marked(text)}></textarea>
+      </div>
     </div>
   );
 }
